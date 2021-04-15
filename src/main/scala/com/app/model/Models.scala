9f8cae1090302fbed9b52809d12a7bfd485e0931
mavic147 <vamaximenko104@gmail.com>
2021-04-15 10:09:20 +0300

    models put into a scala file, not class

--- src/main/scala/com/app/model/Models.scala
+++ src/main/scala/com/app/model/Models.scala
@@ -3,16 +3,12 @@ package com.app.model
 import java.time.LocalDateTime
 import java.util.Date
 
-class Models {
+case class Meal(id: Int, dateTime: LocalDateTime, description: String, calories: Int) {
 
-  case class Meal(id:Int, dateTime: LocalDateTime, description:String, calories:Int) {
+  override def toString: String = "Meal{" + "id=" + id + ", dateTime=" + dateTime + ", description='" + description + '\'' + ", calories=" + calories + '}'
+}
 
-    override def toString: String = "Meal{" + "id=" + id + ", dateTime=" + dateTime + ", description='" + description + '\'' + ", calories=" + calories + '}'
-  }
+case class User(id:Int, name:String, email:String, password:String, caloriesPerDay:Int, registered:Date) {
 
-  case class User(id:Int, name:String, email:String, password:String, caloriesPerDay:Int, registered:Date) {
-
-    override def toString: String = "User{" + "id=" + id + ", email=" + email + ", name=" + name + ", caloriesPerDay=" + caloriesPerDay + '}';
-  }
-
+  override def toString: String = "User{" + "id=" + id + ", email=" + email + ", name=" + name + ", caloriesPerDay=" + caloriesPerDay + '}';
 }
