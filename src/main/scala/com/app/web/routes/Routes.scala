b7253daff56a46cfb80cac453a91464b65458c1f
mavic147 <vamaximenko104@gmail.com>
2021-04-20 18:36:15 +0300

    attempt to handle one request

--- src/main/scala/com/app/web/routes/Routes.scala
+++ src/main/scala/com/app/web/routes/Routes.scala
@@ -1,15 +1,30 @@
 package com.app.web.routes
 
-
-import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
+import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
+import com.app.dao.MealDaoImpl
+import com.app.model.Meal
+import org.json4s.{DefaultFormats, FieldSerializer, Formats}
+import org.mongodb.scala.bson.ObjectId
 
 object MealRoute {
+
+  val mealDaoImpl: MealDaoImpl = MealDaoImpl()
+
+  val mealSerializer = FieldSerializer[Meal]()
+
+  implicit val formats: Formats = DefaultFormats + mealSerializer
+
   val mealRoute: Route = {
     path("meal") {
       get {
-        complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to meal</h1>"))
+        val meal = mealDaoImpl.getOne(new ObjectId("607c966dea9864743f82c11c"),
+          new ObjectId("607c966a0dd06493cc480fd1"))
+        onSuccess(meal) {
+//          case result => complete(result.head)
+          case _      => complete(StatusCodes.NotFound)
+        }
       }
     }
   }
