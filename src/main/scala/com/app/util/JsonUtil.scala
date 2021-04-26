f1f8746700f891dbf4e7b4ec0c62cff7a46d9b6c
mavic147 <vamaximenko104@gmail.com>
2021-04-26 09:59:00 +0300

    one json util removed

--- src/main/scala/com/app/util/JsonUtil.scala
+++ src/main/scala/com/app/util/JsonUtil.scala
@@ -3,5 +3,3 @@ package com.app.util
 case class UserIdJson(userId: String)
 
 case class MealFieldsJson(id: String, dateTime: String, description: String, calories: String)
-
-case class CreateUserJson(name: String, email: String, password: String)
